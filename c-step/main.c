cc6f7a7cef60a322435769cf92ab9390c1e320ee
lvdf <lvdefengyouxiang@126.com>
2020-06-26 09:12:13 +0800

    init

---
+++ c-step/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  c-step
+//
+//  Created by lvdf on 2020/6/26.
+//  Copyright © 2020 lvdf. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
